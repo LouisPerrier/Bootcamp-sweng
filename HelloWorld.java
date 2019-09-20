482478a18898b1f14c407d9e6fc2fbcd19a93f4a
Louis Perrier <contact.louisperrier@gmail.com>
2019-09-20 12:20:11 +0200

    blablah

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
+public static int computeUltimateAnswer() {
+	return 6 * 7;
+}
   public static void main(String[] args) {
-    System.out.println("Hello sweng");
+    System.out.println("Hello sweng " + computeUltimateAnswer());
   }
 }
